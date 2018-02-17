fa618d6cace937f9567742cd3c5fea1d137a4d0c
Konrad Dzwinel <kdzwinel@gmail.com>
2018-02-17 01:05:35 +0100

    Hook up tracker list parser

--- extension/src/background.js
+++ extension/src/background.js
@@ -4,15 +4,38 @@ import * as ABPFilterParser from 'abp-filter-parser';
 
 const browser = window.browser || window.chrome;
 
-const pattern = '<all_urls>';
+const requestTypeMap = new Map([
+  ['script', ABPFilterParser.elementTypes.SCRIPT],
+  ['image', ABPFilterParser.elementTypes.IMAGE],
+  ['stylesheet', ABPFilterParser.elementTypes.STYLESHEET],
+  ['document', ABPFilterParser.elementTypes.DOCUMENT],
+  ['xmlhttprequest', ABPFilterParser.elementTypes.XMLHTTPREQUEST],
+  ['main_frame', ABPFilterParser.elementTypes.DOCUMENT],
+  ['sub_frame', ABPFilterParser.elementTypes.SUBDOCUMENT],
+  ['object_subrequest', ABPFilterParser.elementTypes.OBJECTSUBREQUEST],
+]);
 
-function cancel(requestDetails) {
-  console.log('Canceling: ', requestDetails.url);
-  return { cancel: true };
+const listUrl = browser.extension.getURL('assets/easyprivacy.txt');
+const listData = {};
+
+fetch(listUrl)
+  .then(response => response.text())
+  .then(listText => ABPFilterParser.parse(listText, listData));
+
+function verify(requestDetails) {
+  const initiatorUrl = new URL(requestDetails.initiator);
+  const elementType = requestTypeMap.get(requestDetails.type) || ABPFilterParser.elementTypes.OTHER;
+
+  const cancel = ABPFilterParser.matches(listData, requestDetails.url, {
+    domain: initiatorUrl.domain,
+    elementTypeMaskMap: elementType,
+  });
+
+  return { cancel };
 }
 
 browser.webRequest.onBeforeRequest.addListener(
-  cancel,
-  { urls: [pattern], types: ['image'] },
+  verify,
+  { urls: ['<all_urls>'] },
   ['blocking'],
 );
