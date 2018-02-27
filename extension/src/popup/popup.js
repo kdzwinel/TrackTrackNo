ea4e3f98504921091a4debe35d18f562a97597f0
Konrad Dzwinel <kdzwinel@gmail.com>
2018-02-27 22:33:01 +0100

    Show basic info in the popup.

--- extension/src/popup/popup.js
+++ extension/src/popup/popup.js
@@ -1,10 +1,13 @@
-import {GET_TAB_INFO} from '../messages';
+import { GET_TAB_INFO } from '../messages';
 
 const browser = window.browser || window.chrome;
+const $safelisted = document.querySelector('.js-safelisted');
+const $blocked = document.querySelector('.js-blocked');
 
-window.onload = () => {
-  browser.runtime
-    .sendMessage({ action: GET_TAB_INFO }, (response) => {
-      console.log(response);
-    });
-};
+browser.runtime
+  .sendMessage({ action: GET_TAB_INFO }, (response) => {
+    console.log(response);
+
+    $safelisted.innerText = Boolean(response.safelisted);
+    $blocked.innerText = Array.isArray(response.blocked) ? response.blocked.length : 0;
+  });
